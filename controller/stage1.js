2fef2bfcb3d11172224de5e9539f5db135ec50c3
dunsinadesina <adesinajesudunsin@gmail.com>
2024-07-01 21:46:50 +0100

    greeting a client and extracting their IP address and location

--- controller/stage1.js
+++ controller/stage1.js
@@ -12,14 +12,14 @@ exports.greetVisitor = async (req, res) => {
         location = 'Lagos'; //default location for testing
     } else {
         try {
-            const geoRes = await axios.get(`https://api.ipgeolocation.io/ipgeo?apiKey=${process.env.IPGEOLOCATION_API_KEY}&ip=${clientIpAddress}`);
+            const geoRes = await axios.get(`https://api.ipgeolocation.io/ipgeo?apiKey=0b1b0ca07062407894e5f44ccf28971e&ip=${clientIpAddress}`);
             if (geoRes.data.message === "'127.0.0.1' is a bogon (Internet Assigned Numbers Authority) IP address.") {
                 location = 'localhost';
             } else {
                 location = geoRes.data.city || 'unknown';
                 console.log('Geo data:', geoRes.data);
 
-                const weatherRes = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.OPENWEATHER_API_KEY}&units=metric`);
+                const weatherRes = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=baabf6dbfdad17211b98d497d93b071b&units=metric`);
                 const weatherData = weatherRes.data;
                 temperature = weatherData.main.temp || 'unknown';
                 console.log('Weather data:', weatherData);
